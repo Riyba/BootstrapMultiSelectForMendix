fd8112925e63c6f6fb8185bde3620d3d3add0156
lindski <iain.lindsay@auraq.com>
2015-09-04 16:27:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Bootstrap Multi Select for Mendix
+
+This widget is a wrapper for the [Bootstrap MultiSelect plugin](https://github.com/davidstutz/bootstrap-multiselect)  allowing you to use a dropdown of checkboxes for your reference set.
+
+## Contributing
+
+For more information on contributing to this repository visit [Contributing to a GitHub repository](https://world.mendix.com/display/howto50/Contributing+to+a+GitHub+repository)!
+
+## Typical usage scenario
+
+Where you have a relatively small number of available options for a reference set selector, use this to quickly select the required options from within a dropdown.
+ 
+# Configuration
+
+
+## Data Source
+- Multi Select Source: The reference set association, starting from the dataview object.
+- Display attribute: The attribute to use as the caption for the checkboxes displayed in the dropdown.
+- Data constraint: An XPath constraint, filtering the available objects that are displayed in the dropdown.
+
+## Display
+- Show Label: Whether a label should be displayed for the dropdown.
+- Label Caption: The text to be displayed in the label (only used if Show Label is set to Yes)
+- Form Orientation: Horizontal or Vertical (should match the DataView's Form Orientation value)
+- Label Width: A value between 1 and 11 that determines the width of the label. Will be reset to 1 or 11 if a value is selected that is outside these bounds. (only used if Show Label is set to Yes and Form Orientation is set to Horizontal)
+- Add Select All: Whether the dropdown should be rendered with a 'Select All' option at the top, allowing you to select/deselect all displayed options.
+- Items to display: The max number of items that will be displayed as a comma delimited string in the dropdown field before being replaced with the text 'n selected' (where n is the number of selected items).
+
+## Events
+On change: The microflow that will be run when an item is checked or unchecked.
---
+++ src/BootstrapMultiSelectForMendix/widget/BootstrapMultiSelect.js
@@ -0,0 +1,324 @@
+/*
+    TagsInputForReferenceSet
+    ========================
+
+    @file      : BootstrapMultiSelect.js
+    @version   : 1.0
+    @author    : Iain Lindsay
+    @date      : Wed, 02 Sep 2015 08:08:10 GMT
+    @copyright : AuraQ Limited 2015
+    @license   : Apache v2
+
+    Documentation
+    ========================
+    This widget is a wrapper for the Bootstrap multi select control by David Stutz:
+    https://github.com/davidstutz/bootstrap-multiselect
+    
+    Available options are held in a separate entity, selected items are stored in a reference set association to that entity.
+*/
+
+define([
+    'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_TemplatedMixin', 'dijit/_AttachMixin',
+    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text', 'dojo/html', 'dojo/_base/event',     'BootstrapMultiSelectForMendix/widget/lib/jquery-1.11.2.min',
+    'BootstrapMultiSelectForMendix/widget/lib/bootstrap.min',
+    'BootstrapMultiSelectForMendix/widget/lib/bootstrap-multiselect.min', 'dojo/text!BootstrapMultiSelectForMendix/widget/templates/BootstrapMultiSelect.html'
+], function (declare, _WidgetBase, _TemplatedMixin, _AttachMixin, dom, dojoDom, domQuery, domProp, domClass, domStyle, domConstruct, dojoArray, lang, text, html, event, _jQuery, _bootstrap, _bootstrapMultiSelect, widgetTemplate) {
+    'use strict';
+
+    var $ = jQuery.noConflict(true);
+    
+    return declare('BootstrapMultiSelectForMendix.widget.BootstrapMultiSelect', [_WidgetBase, _TemplatedMixin], {
+
+        templateString: widgetTemplate,
+
+        dataAssociation: "",
+        dataAttribute: "",
+        fieldCaption: "",
+        formOrientation: "",
+
+        _entity: null,      
+        _labelAttribute: null,  
+        _reference: null,
+        _handles: null,
+        _contextObj: null,
+        _$alertdiv: null,
+        _$combo: null,
+        _comboData : [],
+
+        constructor: function () {
+            this._handles = [];
+        },        
+
+        postCreate: function (obj) {
+
+            this._entity = this.dataAssociation.split('/')[1];
+            this._reference = this.dataAssociation.split('/')[0];
+            this._labelAttribute = this.labelAttribute.split('/')[2];
+            
+            // make sure we only select the control for the current id or we'll overwrite previous instances
+            var selector = '#' + this.id + ' select.multiSelect';
+            this._$combo = $(selector);  
+            
+            // adjust the template based on the display settings.
+            if( this.showLabel ) {
+                if(this.formOrientation === "horizontal"){
+                    // width needs to be between 1 and 11
+                    var comboLabelWidth = this.labelWidth < 1 ? 1 : this.labelWidth;
+                    comboLabelWidth = this.labelWidth > 11 ? 11 : this.labelWidth;
+                    
+                    var comboControlWidth = 12 - comboLabelWidth,                    
+                        comboLabelClass = 'col-md-' + comboLabelWidth,
+                        comboControlClass = 'col-md-' + comboControlWidth;
+                    
+                    domClass.add(this.multiSelectLabel, comboLabelClass);
+                    domClass.add(this.multiSelectComboContainer, comboControlClass);
+                }
+
+                this.multiSelectLabel.innerHTML = this.fieldCaption;
+            }
+            else {
+                domClass.remove(this.multiSelectMainContainer, "form-group");
+                domConstruct.destroy(this.multiSelectLabel);
+            }
+        },
+
+        update: function (obj, callback) {
+            
+            var self = this;
+            
+            if (obj === null) {
+                if (!domClass.contains(this.domNode, 'hidden')) {
+                    domClass.add(this.domNode, 'hidden');
+                }
+            } else {
+                if (domClass.contains(this.domNode, 'hidden')) {
+                    domClass.remove(this.domNode, 'hidden');
+                }
+                this._contextObj = obj;
+                this._resetSubscriptions();
+                
+                this._$combo.multiselect({
+                    numberDisplayed : this.itemsToDisplay,
+                    maxHeight: 200, // TODO... make this configurable
+                    includeSelectAllOption: self.addSelectAll,
+                    includeSelectAllDivider: self.addSelectAll,
+                    buttonClass: 'form-control',
+                    dropRight: true,                    
+                    onChange: function(option, checked, select) {
+                        // in the absence of an 'onDeselectAll' event we assume
+                        // its the selectall checkbox if the option value is undefined
+                        if( option === undefined ) {
+                            if (checked){
+                                dojoArray.forEach(self._comboData, function (availableObject, index) {                
+                                    var guid = availableObject.value;
+
+                                    self._contextObj.addReference(self._reference, guid);
+                                });
+                            }
+                            else{
+                               dojoArray.forEach(self._comboData, function (availableObject, index) {                
+                                    var guid = availableObject.value;
+
+                                    self._contextObj.removeReferences(self._reference, [guid]);
+                                });
+                            }                            
+                        }
+                        else {
+                            var guid = $(option).val();
+                            if (checked){
+                                self._contextObj.addReference(self._reference, guid);
+                            }
+                            else{
+                                self._contextObj.removeReferences(self._reference, [guid]);
+                            }
+                        }
+                        
+                        // run the OC microflow if one has been configured.                   
+                        if( self.onChangeMicroflow ) {
+                            self._execMf(self._contextObj.getGuid(), self.onChangeMicroflow);
+                        }
+                    }
+                });                            
+                
+                // load the available options and ticks the already associated options.
+                this._loadComboData();
+            }
+
+            // Execute callback.
+            if (typeof callback !== 'undefined') {
+                callback();
+            }
+        },
+
+        enable: function () {},
+
+        disable: function () {},
+
+        resize: function (box) {},
+
+        uninitialize: function () {},
+        
+        // retrieves the data from the child entity, applying the required constraint
+        _loadComboData: function () {
+            
+            // reset our data
+            this._comboData = [];
+            
+            var xpath = '//' + this._entity + this.dataConstraint.replace('[%CurrentObject%]', this._contextObj.getGuid());
+            mx.data.get({
+                xpath: xpath,
+                filter: {
+                    sort: [[this._labelAttribute, "asc"]],
+                    offset: 0
+                },
+                callback: lang.hitch(this, this._processComboData)
+            });
+        },
+        
+        // sets up the available combo options
+        _processComboData: function (objs) {
+            var self = this;
+            // reset our data
+            self._comboData = [];            
+            
+            dojoArray.forEach(objs, function (availableObject, index) {
+                
+                var optionLabel = dom.escapeString(availableObject.get(self._labelAttribute)),
+                    optionValue = availableObject.getGuid(),
+                    item = {
+                        label: optionLabel, value: optionValue, selected: false
+                    };    
+                
+                self._comboData.push(item);
+            });
+            
+            // load and set the current associated data
+            self._setReferencedObjects();            
+        },
+
+        // marks referenced objects as selected in the combo options.
+        _setReferencedObjects: function () {           
+            var self = this,
+                referencedObjects = this._contextObj.get(this._reference);
+            
+            if(referencedObjects !== null && referencedObjects !== "") {
+                dojoArray.forEach(self._comboData, function (availableObject, index) {                
+                    //check if this is a current tag
+                    dojoArray.forEach(referencedObjects, function (ref, i) {
+                        if (availableObject.value === ref) {
+                                availableObject.selected = true;
+                        }
+                    }, this);
+                }, this);
+            }
+            
+            // update array and set selected flag based on referenced options                
+            self._$combo.multiselect('dataprovider', this._comboData);                
+        },
+
+        _execMf: function (guid, mf, cb) {
+            if (guid && mf) {
+                mx.data.action({
+                    applyto: 'selection',
+                    actionname: mf,
+                    guids: [guid],
+                    callback: function () {
+                        if (cb) {
+                            cb();
+                        }
+                    },
+                    error: function (e) {
+                        console.error('Error running Microflow: ' + e);
+                    }
+                }, this);
+            }
+
+        },
+
+        _resetSubscriptions: function () {
+            // Release handle on previous object, if any.
+            var handle = null,
+                attrHandle = null,
+                validationHandle= null;
+
+            if (this._handles) {
+                dojoArray.forEach(this._handles, function (handle) {
+                    this.unsubscribe(handle);
+                });
+                this._handles = [];
+            }
+
+            if (this._contextObj) {
+                handle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: function (guid) {
+                        mx.data.get({
+                            guid: guid,
+                            callback: lang.hitch(this, function (obj) {
+                                this._contextObj = obj;
+                                this._loadComboData();
+                            })
+                        });
+
+                    }
+                });
+                attrHandle = mx.data.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.attribute,
+                    callback: lang.hitch(this, function (guid) {
+                        mx.data.get({
+                            guid: guid,
+                            callback: lang.hitch(this, function (obj) {
+                                this._contextObj = obj;
+                                this._loadComboData();
+                            })
+                        });
+                    })
+                });
+
+
+                validationHandle = mx.data.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: lang.hitch(this, this._handleValidation)
+                });
+
+                this._handles.push(handle);
+                this._handles.push(attrHandle);
+                this._handles.push(validationHandle);
+            }
+        },
+        
+        _handleValidation: function (validations) {
+
+            this._clearValidations();
+
+            var val = validations[0],
+                msg = val.getReasonByAttribute(this._reference);
+
+            if (this.readOnly) {
+                val.removeAttribute(this._reference);
+            } else {
+                if (msg) {
+                    this._addValidation(msg);
+                    val.removeAttribute(this._reference);
+                }
+            }
+        },
+
+        _clearValidations: function () {
+            if( this._$alertdiv ) {
+                this._$combo.parent().removeClass('has-error');
+                this._$alertdiv.remove();
+            }
+        },
+
+        _addValidation: function (msg) {            
+            this._$alertdiv = $("<div></div>").addClass('alert alert-danger mx-validation-message').html(msg);
+            this._$combo.parent().addClass('has-error').append( this._$alertdiv );            
+        }
+    });
+});
+require(['BootstrapMultiSelectForMendix/widget/BootstrapMultiSelect'], function () {
+    'use strict';
+});
